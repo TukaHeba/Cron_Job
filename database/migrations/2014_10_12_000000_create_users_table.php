3348ebf410755204dee0d4219b4d707455b5b558
Tuka Heba <tuka.heba@gmail.com>
2024-10-23 23:22:36 +0300

    Migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,13 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
+
+            // Indexes
+            $table->index('is_admin');
+            $table->index('email');
         });
     }
 
