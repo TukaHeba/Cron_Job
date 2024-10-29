8fa2f5843aefe71d9f842f9331ed5609e2d338b5
Tuka Heba <tuka.heba@gmail.com>
2024-10-29 17:24:17 +0300

    Add Chaching forTasks

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -4,30 +4,35 @@ namespace App\Services;
 
 use App\Models\Task;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 
 class TaskService
 {
     /**
      * List all tasks if the auth user is admin, otherwise only tasks assigned to the auth user.
+     * Define a cache key and cache for 60 minutes.
      * 
      * @param mixed $user
      * @return \Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
      */
     public function listAllTasks($user)
     {
-        if ($user->is_admin) {
-            return Task::with(['assignedTo', 'createdBy'])->get();
-        }
+        $cacheKey = 'tasksByUser_' . $user->id;
 
-        $tasks = Task::with(['assignedTo', 'createdBy'])
-            ->where('assigned_to', $user->id)
-            ->orWhere('created_by', $user->id)->get();
+        return Cache::remember($cacheKey, 60, function () use ($user) {
+            if ($user->is_admin) {
+                return Task::with(['assignedTo', 'createdBy'])->get();
+            }
 
-        return $tasks;
+            $tasks = Task::with(['assignedTo', 'createdBy'])
+                ->where('assigned_to', $user->id)
+                ->orWhere('created_by', $user->id)->get();
+            return $tasks;
+        });
     }
 
     /**
-     * Create a new task and assign the authenticated user as the creator.
+     * Create a new task and assign the authenticated user as the creator & clear the cache for the user.
      * 
      * @param array $data
      * @return Task|\Illuminate\Database\Eloquent\Model
@@ -35,11 +40,15 @@ class TaskService
     public function createTask(array $data)
     {
         $data['created_by'] = Auth::id();
-        return Task::create($data);
+        $task = Task::create($data);
+
+        Cache::forget('tasksByUser_' . Auth::id());
+
+        return $task;
     }
 
     /**
-     * Update the specified task with the given data.
+     * Update the specified task with the given data and clear the cache for the user.
      * 
      * @param \App\Models\Task $task
      * @param array $data
@@ -48,11 +57,15 @@ class TaskService
     public function updateTask(Task $task, array $data)
     {
         $task->update(array_filter($data));
+
+        Cache::forget('tasksByUser_' . $task->assigned_to);
+        Cache::forget('tasksByUser_' . $task->created_by);
+
         return $task;
     }
 
     /**
-     * Delete the specified task.
+     * Delete the specified task and clear the cache for the user.
      * 
      * @param \App\Models\Task $task
      * @return void
@@ -60,6 +73,9 @@ class TaskService
     public function deleteTask(Task $task)
     {
         $task->delete();
+
+        Cache::forget('tasksByUser_' . $task->assigned_to);
+        Cache::forget('tasksByUser_' . $task->created_by);
     }
 
     /**
